25e0c7f4ea22bee673368ca81237c853bd4630d1
Marcelo Silva <marcelofelipebd@gmail.com>
2019-09-22 11:35:39 -0300

    Presidente

--- main.java
+++ main.java
@@ -1,7 +1,9 @@
 public class Main{
     public static void main(String[] args){
-        System.out.println("==============");
-        System.out.println("  Voa Unect   ");
-        System.out.println("==============");
+        System.out.println("===================");
+        System.out.println("     Voa Unect     ");
+        System.out.println("===================");
+        System.out.println("  GUI EH O PRESIDA ");
+        System.out.println("===================");
     }
 }
